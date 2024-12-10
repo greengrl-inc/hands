2c94bb01ea12da0604167b2f1034304d38dc242e
Nico - <aabautista70@gmail.com>
2024-12-10 14:28:15 -0500

    draw hands

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function preload() {
     handPose = ml5.handPose({ flipped: true });
 }
 
-function mousePressed(){
+function mousePressed() {
     console.log(hands);
 }
 
@@ -23,21 +23,40 @@ function setup() {
 
 function draw() {
     background(25);
-    // imageMode(CENTER);
     image(video, 0, 0, 640, 480);
 
     // Draw all the tracked hand points
     for (let i = 0; i < hands.length; i++) {
         let hand = hands[i];
-        if (hand.confidence > 0.5) {
-        for (let j = 0; j < hand.keypoints.length; j++) {
-            let keypoint = hand.keypoints[j];
-            fill(255, 255, 255);
-            noStroke();
-            circle(keypoint.x, keypoint.y, 10);
+        if (hand.confidence > 0.7) {
+            for (let j = 0; j < hand.keypoints.length; j++) {
+                let keypoint = hand.keypoints[j];
+                fill(255, 255, 255);
+                noStroke();
+                circle(keypoint.x, keypoint.y, 5);
+            }
+
+            // circle between fingers -  coding train 
+
+            if (hand.handedness == "Left") {
+                let index = hand.index_finger_tip;
+                let thumb = hand.thumb_tip;
+
+                let x = (index.x + thumb.x) / 2; // average 
+                let y = (index.y + thumb.y) / 2; // average 
+
+                let d = dist(index.x, index.y, thumb.x, thumb.y);
+
+                if (d > 22) {
+                    circle(x, y, d);
+                }
+            }
+
+
+
         }
     }
-    }
+
 }
 
 // Callback function for when handPose outputs data
