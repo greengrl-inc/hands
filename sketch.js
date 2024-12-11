6c5aa19dcefeef6e5c924d1e1072e791fc49775f
Nico - <aabautista70@gmail.com>
2024-12-10 21:52:20 -0500

    osc

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
 let handPose;
 let video;
 let hands = [];
+let d;
+let osc, playing, frq, amp;
 
 function preload() {
     handPose = ml5.handPose({ flipped: true });
 }
 
 function mousePressed() {
-    console.log(hands);
+    console.log(hands, d);
 }
 
 // https://p5js.org/reference/p5/createCapture/
@@ -19,25 +21,20 @@ function setup() {
 
     // Start detecting hands from the webcam video
     handPose.detectStart(video, gotHands);
+
+    osc = new p5.Oscillator();
 }
 
 function draw() {
     background(25);
     image(video, 0, 0, 640, 480);
 
-    // Draw all the tracked hand points
+    // Draw 
     for (let i = 0; i < hands.length; i++) {
         let hand = hands[i];
         if (hand.confidence > 0.7) {
-            for (let j = 0; j < hand.keypoints.length; j++) {
-                let keypoint = hand.keypoints[j];
-                fill(255, 255, 255);
-                noStroke();
-                circle(keypoint.x, keypoint.y, 5);
-            }
 
-            // circle between fingers -  coding train 
-
+            // volume - codingtrain
             if (hand.handedness == "Left") {
                 let index = hand.index_finger_tip;
                 let thumb = hand.thumb_tip;
@@ -45,14 +42,26 @@ function draw() {
                 let x = (index.x + thumb.x) / 2; // average 
                 let y = (index.y + thumb.y) / 2; // average 
 
-                let d = dist(index.x, index.y, thumb.x, thumb.y);
+                d = dist(index.x, index.y, thumb.x, thumb.y);
 
-                if (d > 22) {
+                if (d > 20) {
+                    osc.start();
                     circle(x, y, d);
+                } else {
+                    osc.stop();
                 }
+
+                amp = map(d, 0, 200, 0, 1);
+                osc.amp(round(amp, .5));
             }
 
-
+            // pitch
+            if (hand.handedness == "Right") {
+                let index = hand.index_finger_tip.y;
+                
+                frq = constrain(map(index, 0, height, 100, 500), 100, 500);
+                osc.freq(frq, 0.1);
+            }
 
         }
     }
