a1c7c3bc65cf04ad2da83364b66ab73a1f6eb68f
Nico - <aabautista70@gmail.com>
2024-12-16 13:00:34 -0500

    osc start once + conf

--- sketch.js
+++ sketch.js
@@ -32,41 +32,51 @@ function draw() {
     // Draw all the tracked hand points
     for (let i = 0; i < hands.length; i++) {
         let hand = hands[i];
-        for (let j = 0; j < hand.keypoints.length; j++) {
-            let keypoint = hand.keypoints[j];
-            fill(186, 255, 223, 200);
-            // 
-            noStroke();
-            circle(keypoint.x + 260, keypoint.y, 5);
-        }
-
-        if (hand.handedness == "Left") {
-            let index = hand.index_finger_tip;
-            let thumb = hand.thumb_tip;
-
-            let x = (index.x + thumb.x) / 2; // average dist
-            let y = (index.y + thumb.y) / 2; // average dist
-
-            d = dist(index.x, index.y, thumb.x, thumb.y);
-
-            if (d > 25) {
+        if (hand.confidence > 0.7) {
+            // https://p5js.org/tutorials/simple-melody-app/
+            // only start once
+            if (!osc.started) {
                 osc.start();
-                fill(150, 230, 179, 230);
-                circle(x + 260, y, d * .87);
-            } else {
-                osc.stop();
+            }
+            for (let j = 0; j < hand.keypoints.length; j++) {
+                let keypoint = hand.keypoints[j];
+                fill(186, 255, 223, 200);
+                // 
+                noStroke();
+                circle(keypoint.x + 260, keypoint.y, 5);
             }
 
-            amp = map(d, 25, 200, 0, 1, true);
-            osc.amp(round(amp, 1));
-        }
+            if (hand.handedness == "Left") {
+                let index = hand.index_finger_tip;
+                let thumb = hand.thumb_tip;
 
-        if (hand.handedness == "Right") {
-            let indexY = hand.index_finger_tip.y;
-            frq = map(indexY, 0, 480, 500, 100, true);
-            osc.freq(frq);
-        }
+                let x = (index.x + thumb.x) / 2; // average dist
+                let y = (index.y + thumb.y) / 2; // average dist
 
+                d = dist(index.x, index.y, thumb.x, thumb.y);
+                d = Math.round(d / 10) * 10;
+
+
+                if (d > 25) {
+                    fill(150, 230, 179, 230);
+                    circle(x + 260, y, d);
+                } else {
+                    osc.amp(0);
+                    circle(x + 260, y, 0);
+                }
+
+                amp = map(d, 25, 200, 0, 1, true);
+                amp = round(amp, 2);
+                osc.amp(amp);
+            }
+
+            if (hand.handedness == "Right") {
+                let indexY = hand.index_finger_tip.y;
+                frq = map(indexY, 0, 480, 500, 100, true);
+                osc.freq(frq);
+            }
+
+        }
     }
 
 }
